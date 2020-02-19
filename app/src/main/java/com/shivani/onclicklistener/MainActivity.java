4614b271f0c55eb7fd8b9b48c89c508d785cfb87
Shivani Puli <shivanipuli@gmail.com>
2020-02-19 14:14:27 -0500

    buttons each have different count

--- app/src/main/java/com/shivani/onclicklistener/MainActivity.java
+++ app/src/main/java/com/shivani/onclicklistener/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         button3=findViewById(R.id.bottomLeft);
         button4=findViewById(R.id.bottomRight);
         editor = sharedPreferences.edit();
-        editor.putInt("press",1);
-        editor.apply();
+//        editor.putInt("press",1);
+//        editor.apply();
         seekBar=findViewById(R.id.seekBar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -56,13 +56,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public void press(View v){
-        Log.i("button", "Button Pressed");
+    public void setButton1(View v){
+        Log.i("button", "Button 1 Pressed");
         //toast.show();
-        int value = sharedPreferences.getInt("press", 3) + 1;
-        editor.putInt("press",value);
+        int value = sharedPreferences.getInt("button1", 0) + 1;
+        editor.putInt("button1",value);
         editor.apply();
-        String string="Button was pressed " + value + " times.";
+        String string="Button 1 was pressed " + value + " times.";
+        Toast.makeText(getApplicationContext(),string,Toast.LENGTH_SHORT).show();
+    }
+    public void setButton2(View v){
+        Log.i("button", "Button 2 Pressed");
+        //toast.show();
+        int value = sharedPreferences.getInt("button2", 0) + 1;
+        editor.putInt("button2",value);
+        editor.apply();
+        String string="Button 2 was pressed " + value + " times.";
+        Toast.makeText(getApplicationContext(),string,Toast.LENGTH_SHORT).show();
+    }
+    public void setButton3(View v){
+        Log.i("button", "Button 3 Pressed");
+        //toast.show();
+        int value = sharedPreferences.getInt("button3", 0) + 1;
+        editor.putInt("button3",value);
+        editor.apply();
+        String string="Button 3 was pressed " + value + " times.";
+        Toast.makeText(getApplicationContext(),string,Toast.LENGTH_SHORT).show();
+    }
+    public void setButton4(View v){
+        Log.i("button", "Button 4 Pressed");
+        //toast.show();
+        int value = sharedPreferences.getInt("button4", 0) + 1;
+        editor.putInt("button4",value);
+        editor.apply();
+        String string="Button 4 was pressed " + value + " times.";
         Toast.makeText(getApplicationContext(),string,Toast.LENGTH_SHORT).show();
     }
 }
